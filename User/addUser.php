c3a3d6af9d6e888f43b72b76ed383e2fb424c2a5
SiRu <yeesiru1205@gmail.com>
2024-10-31 22:49:19 +0800

    modify adduser

--- User/addUser.php
+++ User/addUser.php
@@ -1,3 +1,50 @@
+<?php
+include("../database/db_conn.php");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $school = $_POST['school'];
+    $role = $_POST['role'];
+
+        //Insert the new user entry into the database
+        $sql = "INSERT INTO user (username, password, name, gender, email, phone, school, role) VALUES ('$username', '$password', '$name','$gender', '$email', '$phone', '$school', '$role')";
+        if ($conn->query($sql) === TRUE) {
+            echo "<script>
+                document.addEventListener('DOMContentLoaded', function () {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'User entry added successfully.',
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = 'userManage.html';
+                        }
+                    });
+                });
+            </script>";
+        } 
+    } else {
+        echo "<script>
+            Swal.fire({
+                title: 'Error!',
+                text: 'Error adding new user record: " . $conn->error . "',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+        </script>";
+    }
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -7,7 +54,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/navigation.css">    
-    <link rel="stylesheet" href="../css/addUser.css">
+    <link rel="stylesheet" href="../css/manageUser.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
@@ -21,7 +68,7 @@
         <h1 style="text-align: center;">Add New User</h1>
 
             <div class="user-table justify-content-center">
-                <form id="addUserForm" action=" " method="POST" enctype="multipart/form-data"> 
+                <form id="addUserForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" enctype="multipart/form-data"> 
                     <div class="form-group user-input">
                         <label for="username" class="form-label">Username: </label>
                         <input type="text" id="username" name="username" required>
@@ -39,8 +86,8 @@
 
                     <div class="form-group">
                         <label for="gender">Gender: </label>
-                        <input type="radio" name="gender" value="female">Female
-                        <input type="radio" name="gender" value="male">Male
+                        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female" ) echo "checked";?> value="female">Female
+                        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male" ) echo "checked";?> value="male">Male
                     </div>
 
                     <div class="form-group user-input">
@@ -74,6 +121,10 @@
                 </form>
             </div>
         </div>
+
+    <?php
+    $conn->close();
+    ?>
 
 </body>
 
