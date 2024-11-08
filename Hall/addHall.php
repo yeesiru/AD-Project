cd8e8e37ba13dd060829a2b504067fb3081d7899
hadifirdaus14 <hadi.firdaus@graduate.utm.my>
2024-11-08 23:23:08 +0800

    .

--- Hall/addHall.php
+++ Hall/addHall.php
@@ -51,6 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">   
+    <link rel="stylesheet" href="../css/navigation.css"> 
     <link rel="stylesheet" href="../css/hall.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
