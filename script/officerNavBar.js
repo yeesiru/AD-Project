75bdfe62800b68bc472032520915f7d217ff6369
Mohamedarshathsaleem <mohamedarshathsaleem@gmail.com>
2024-12-25 17:39:37 +0800

    add viewAmbulance

--- script/officerNavBar.js
+++ script/officerNavBar.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <li> <a class="sub-btn">Our Services &#x25BE;</a>
                 <ul class="sidebar-dropdown">
                     <li><a href="#" class="sub-item">Hall Booking</a></li>
-                    <li><a href="#" class="sub-item">Ambulance Booking</a></li>
+                    <li><a href="../ambulance/manageAmbulanceBooking.php" class="sub-item">Ambulance Booking</a></li>
                     <li><a href="../Equipment_user/manageEquipmentBooking.php" class="sub-item">Equipment Booking</a></li>
                 </ul>
             </li>
